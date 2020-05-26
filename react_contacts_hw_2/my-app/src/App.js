6c431e183a6d1bb701e5c36b8c0e689cc750f886
kris30may1 <khrystyna.korzh@gmail.com>
2020-05-26 13:42:59 +0300

    hw_2

---
+++ react_contacts_hw_2/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    )
+  }
+}
